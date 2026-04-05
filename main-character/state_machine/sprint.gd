03a4b938c7593443f07961fa315aa0e5918b1080
GiFlorin <giovannaflorin07@gmail.com>
2026-04-05 18:44:25 -0300

    placeholder animations for character

--- main-character/state_machine/sprint.gd
+++ main-character/state_machine/sprint.gd
@@ -4,8 +4,17 @@ func enter() -> void:
 	super()
 
 func process_frame(_delta: float) -> State:
-	if Input.is_action_pressed('move_left'):
+	parent.animations.play(animation_name)
+	if Input.is_action_just_pressed("move_down"):
+		animation_name = 'sprint_down'
+		parent.animations.flip_h = false
+	if Input.is_action_pressed("move_left"):
+		animation_name = 'sprint_side'
 		parent.animations.flip_h = true
-	else:
+	if Input.is_action_pressed('move_right'):
+		animation_name = 'sprint_side'
+		parent.animations.flip_h = false
+	if Input.is_action_pressed("move_up"):
+		animation_name = 'sprint_up'
 		parent.animations.flip_h = false
 	return null
--- main-character/state_machine/walk.gd
+++ main-character/state_machine/walk.gd
@@ -7,11 +7,19 @@ var vel_y = 0
 
 func enter() -> void:
 	super()
-	
 
 func process_frame(delta: float) -> State:
-	if Input.is_action_pressed('move_left'):
+	parent.animations.play(animation_name)
+	if parent.input_direction:
+		animation_name = 'walk_up'
+		parent.animations.flip_h = false
+	if Input.is_action_just_pressed("move_down"):
+		animation_name = 'walk_down'
+		parent.animations.flip_h = false
+	if Input.is_action_pressed("move_left"):
+		animation_name = 'walk_side'
 		parent.animations.flip_h = true
-	else:
+	if Input.is_action_pressed('move_right'):
+		animation_name = 'walk_side'
 		parent.animations.flip_h = false
 	return null
